b9fb899a2bedd41d7a80e7aac5763bd2316565d4
bean-vip <bean_vip@126.com>
2017-05-31 06:56:37 +0800

    add a article or modify resources

---
+++ dp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "start deploy my blog..."
+git status
+git add *
+git status
+git commit -m "add a article or modify resources"
+git push origin master
+echo "successful commit to blog"
