38caf970afc26e8c67a455d2b1f9237fff84d05f
blair <blair.values@gmail.com>
2018-10-07 13:55:52 +0800

    add a article or modify resources

--- dp.sh
+++ dp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "start deploy my blog..."
+echo "start deploy my blog source..."
 git pull
 git status
 git add *
 git status
 git commit -m "add a article or modify resources"
 git push origin master
-echo "successful commit to blog"
+echo "successful commit to blog source"
